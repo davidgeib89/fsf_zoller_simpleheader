16edd3c5accdeb0ce2dd7bcf9c185e5bf543ce5e
David Geib <d.geib@freesixtyfive.de>
2021-01-04 16:26:51 +0100

    slider scrolldown video etc

--- Resources/Public/js/main.js
+++ Resources/Public/js/main.js
@@ -1,3 +1,15 @@
-$( document ).ready(function() {
-	
+$(document).ready(function(){
+	$('.slider-container-simpleheader').slick({
+	});
+
+	$(document).on("click", ".next-viewport-downn", function (event) {
+		event.preventDefault();
+		var vheight = $(window).height();
+		$("html, body").animate(
+		  {
+			scrollTop: ((Math.floor($(window).scrollTop() / vheight) + 1) * vheight)+142,
+		  },
+		  500
+		);
+	  });
 });
